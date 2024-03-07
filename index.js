0496d7cdd06db14e4da8bde3a130d2e3dfad7276
krunal5291 <krunalkalathiya11@gmail.com>
2024-03-07 18:38:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function setFirstName() {
+    document.getElementById("firstname").innerHTML="krunal"
+  }
+
+  function setLastName() {
+    document.getElementById("lastname").innerHTML="kalathiya"
+  }
+
+  function setFullName() {
+    document.getElementById("fullname").innerHTML="krunal arvindbhai kalathiya"
+  }
+
+  function resetDefaults() {
+    document.getElementById("firstname").innerHTML=""
+    document.getElementById("lastname").innerHTML=""
+    document.getElementById("fullname").innerHTML=""
+
+  }
